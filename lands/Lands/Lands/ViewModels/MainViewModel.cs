75252cf550b9e08acca6879b3f275ec9114e2f8b
Jose Mayorga <josepabon8@gmail.com>
2018-07-27 11:37:06 -0500

    Login View Model

--- lands/Lands/Lands/ViewModels/MainViewModel.cs
+++ lands/Lands/Lands/ViewModels/MainViewModel.cs
@@ -6,6 +6,14 @@ namespace Lands.ViewModels
 {
     public class MainViewModel
     {
-
+        #region ViewModel
+        public LoginViewModel Login { get; set; }
+        #endregion
+        #region Constructors
+        public MainViewModel()
+        {
+            this.Login = new LoginViewModel();
+        }
+        #endregion
     }
 }
