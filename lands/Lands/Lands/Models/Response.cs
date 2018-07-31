2db770b2d704717cecc89764dcebae6b4f05605e
Jose Mayorga <josepabon8@gmail.com>
2018-07-31 15:36:20 -0500

    check connection

---
+++ lands/Lands/Lands/Models/Response.cs
@@ -0,0 +1,11 @@
+﻿namespace Lands.Models
+{
+    public class Response
+    {
+        /*si IsSuccess viene true Result trae los datos de lo contrario
+         * mostrará el Message  */
+        public bool IsSuccess { get; set; }
+        public string Message { get; set; }
+        public object Result { get; set; }
+    }
+}
--- lands/Lands/Lands/ViewModels/LandsViewModel.cs
+++ lands/Lands/Lands/ViewModels/LandsViewModel.cs
@@ -1,7 +1,68 @@
 ﻿namespace Lands.ViewModels
 {
-    public class LandsViewModel
-    {
+    using Lands.Models;
+    using Lands.Services;
+    using System.Collections.Generic;
+    using System.Collections.ObjectModel;
 
+    public class LandsViewModel : BaseViewModel
+    {
+        #region Services
+        private ApiService apiService;
+        #endregion
+
+        #region Attributes
+        //es observable para poderla pintar en un listview
+        private ObservableCollection<Land> lands;
+        #endregion
+
+        #region Properties
+        public ObservableCollection<Land> Lands
+        {
+            get { return this.lands;}
+            set { SetValue(ref this.lands, value); }
+        }
+        #endregion
+
+        #region Constructors
+        public LandsViewModel()
+        {
+            this.LoadLands();
+        }
+        #endregion
+
+        #region Methods
+        private async void LoadLands()
+        {
+            //Verificamos que haya concexión a internet
+            var connection = await this.apiService.CheckConnection();
+            if (!connection.IsSuccess)
+            {
+                //si no hay conexion
+                await App.Current.MainPage.DisplayAlert("Error", connection.Message, "Accept");
+
+                //es como dar flecha atras en la navegación nos devolvemos de la page
+                await App.Current.MainPage.Navigation.PopAsync();
+                return;
+            }
+
+            /*le enviamos al metodo getlist los parámetros 
+             * urlbase = ttp://www.restcountries.eu
+             * prefijo = /rest
+             * Controlador = /v2/all  */
+            var response = await this.apiService.GetList<Land>(
+                "ttp://www.restcountries.eu",
+                "/rest", 
+                "/v2/all");
+            //si conectó y devolvió los valores de la api
+            if (!response.IsSuccess)
+            {
+                await App.Current.MainPage.DisplayAlert("Error", response.Message, "Accept");
+                return;
+            }
+            var list = (List<Land>)response.Result;
+            this.Lands = new ObservableCollection<Land>(list);
+        }
+        #endregion
     }
 }
